bbe57b1be4a85e51bce3f884e1a9d9bf795c5179
kuomolew <kuomolew@gmail.com>
2024-01-14 21:54:48 +0100

    update tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
       fontFamily: {
         sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
       },
+      boxShadow: {
+        green: '0 0 3px 3px rgb(74 222 128)',
+        gray: '0 1px 3px 0 rgba(60, 64, 67, 0.3)',
+      },
     },
   },
   plugins: [],
