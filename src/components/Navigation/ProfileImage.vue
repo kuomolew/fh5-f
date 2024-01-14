a454babd0956b8042456d17782fabf851f02dabc
kuomolew <kuomolew@gmail.com>
2024-01-14 21:57:07 +0100

    add ProfileImage component

---
+++ src/components/Navigation/ProfileImage.vue
@@ -0,0 +1,10 @@
+<template>
+  <img :src="imageLink" alt="User Profile Image" class="h-8 w-8 rounded-3xl object-contain" />
+</template>
+
+<script lang="ts" setup>
+import image from '@/common/images/defaultUser.png';
+import { ref } from 'vue';
+
+const imageLink = ref(image);
+</script>
