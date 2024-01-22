0cc25ca60d3d8d0f77fd2263ba7378c1bd882255
kuomolew <kuomolew@gmail.com>
2024-01-22 23:33:47 +0100

    add ManufacturerSelection component

---
+++ src/components/Selection/ManufacturerSelection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="flex flex-col border-r border-solid border-brand-gray-200 bg-white p-4 w-80 h-full min-h-screen"
+  >
+    <h1 class="w-full text-center text-xl font-medium">Make:</h1>
+    <manufacturer-card
+      v-for="manufacturer in manufacturers"
+      :key="manufacturer"
+      :is-active="false"
+      :manufacturer="manufacturer"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useManufacturersStore } from '@/stores/manufacturers';
+import ManufacturerCard from '@/components/Cards/ManufacturerCard.vue';
+
+const manufacturersStore = useManufacturersStore();
+
+let manufacturers = computed(() => manufacturersStore.FILTERED_MANUFACTURERS);
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <main>
     <manufacturer-letter-selection />
+    <manufacturer-selection />
   </main>
 </template>
 
 <script lang="ts" setup>
 import ManufacturerLetterSelection from '@/components/Selection/ManufacturerLetterSelection.vue';
+import ManufacturerSelection from '@/components/Selection/ManufacturerSelection.vue';
 </script>
