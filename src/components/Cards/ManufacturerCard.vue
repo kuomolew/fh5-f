ef1b55b9f95014782335ba5e0c3ff2b6f336e334
kuomolew <kuomolew@gmail.com>
2024-01-22 23:33:30 +0100

    add ManufacturerCard component

---
+++ src/components/Cards/ManufacturerCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <button :class="{ active: isActive }">
+    {{ manufacturer }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  manufacturer: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style scoped>
+button {
+  @apply px-5 py-3 my-4 font-medium rounded text-white bg-green-500 hover:shadow-green;
+}
+
+.active {
+  @apply bg-white text-green-500 border border-green-500 rounded hover:shadow-none;
+}
+</style>
