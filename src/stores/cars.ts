cd77c8363fa145f8db29a649be45d87e761b2041
kuomolew <kuomolew@gmail.com>
2024-01-24 22:18:49 +0100

    update cars store: add filter by manufacturer, tests

--- src/stores/cars.ts
+++ src/stores/cars.ts
@@ -1,8 +1,8 @@
 import { defineStore } from 'pinia';
 
-import getCars from '@/api/getCars';
-
 import type { Car } from '@/api/types';
+import getCars from '@/api/getCars';
+import { useUserStore } from '@/stores/user';
 
 export interface CarsState {
   cars: Car[];
@@ -12,6 +12,7 @@ export const useCarsStore = defineStore('cars', {
   state: (): CarsState => ({
     cars: [],
   }),
+
   actions: {
     async FETCH_CARS() {
       const cars = await getCars();
@@ -20,7 +21,18 @@ export const useCarsStore = defineStore('cars', {
   },
 
   getters: {
+    INCLUDE_CAR_BY_MANUFACTURER: () => (car: Car) => {
+      const userStore = useUserStore();
+
+      if (userStore.selectedManufacturer.length === 0) return false;
+      return userStore.selectedManufacturer === car.manufacturer;
+    },
+
     FILTERED_CARS(state): Car[] {
+      return state.cars.filter((car) => this.INCLUDE_CAR_BY_MANUFACTURER(car));
+    },
+
+    ALL_CARS(state): Car[] {
       return state.cars;
     },
   },
--- tests/unit/stores/cars.test.ts
+++ tests/unit/stores/cars.test.ts
@@ -3,7 +3,9 @@ import type { Mock } from 'vitest';
 
 import axios from 'axios';
 
+import type { Car } from '@/api/types';
 import { useCarsStore } from '@/stores/cars';
+import { useUserStore } from '@/stores/user';
 import { createCar } from '../../utils/createCar';
 
 vi.mock('axios');
@@ -43,14 +45,74 @@ describe('getters', () => {
     setActivePinia(createPinia());
   });
 
+  describe('INCLUDE_CAR_BY_MANUFACTURER', () => {
+    describe('when none manufacturer was selected', () => {
+      it('includes none of cars', () => {
+        const car1 = createCar({ manufacturer: 'Nissan' });
+        const car2 = createCar();
+        const car3 = createCar({ manufacturer: 'Mitsubishi' });
+
+        const cars: Car[] = [car1, car2, car3];
+
+        const userStore = useUserStore();
+        userStore.selectedManufacturer = '';
+
+        const store = useCarsStore();
+
+        const result: boolean[] = [];
+
+        cars.forEach((car) => result.push(store.INCLUDE_CAR_BY_MANUFACTURER(car)));
+
+        expect(result).toEqual([false, false, false]);
+      });
+    });
+
+    describe('when exact manufacturer was selected', () => {
+      it('includes cars from that manufacturer only', () => {
+        const car1 = createCar({ manufacturer: 'Nissan' });
+        const car2 = createCar({ manufacturer: 'Peugeot' });
+        const car3 = createCar({ manufacturer: 'Mitsubishi' });
+        const car4 = createCar({ manufacturer: 'Peugeot' });
+
+        const cars: Car[] = [car1, car2, car3, car4];
+
+        const userStore = useUserStore();
+        userStore.selectedManufacturer = 'Peugeot';
+
+        const store = useCarsStore();
+
+        const result: boolean[] = [];
+
+        cars.forEach((car) => result.push(store.INCLUDE_CAR_BY_MANUFACTURER(car)));
+
+        expect(result).toEqual([false, true, false, true]);
+      });
+    });
+  });
+
   describe('FILTERED_CARS', () => {
     it('gets all filtered cars', () => {
+      const car1 = createCar({ manufacturer: 'Nissan' });
+      const car2 = createCar();
+
+      const userStore = useUserStore();
+      userStore.selectedManufacturer = 'Nissan';
+
+      const store = useCarsStore();
+      store.cars.push(car1, car2);
+
+      expect(store.FILTERED_CARS).toEqual([car1]);
+    });
+  });
+
+  describe('ALL_CARS', () => {
+    it('gets all cars', () => {
       const car1 = createCar();
       const car2 = createCar();
       const store = useCarsStore();
       store.cars.push(car1, car2);
 
-      expect(store.FILTERED_CARS).toEqual([car1, car2]);
+      expect(store.ALL_CARS).toEqual([car1, car2]);
     });
   });
 });
