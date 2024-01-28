60e4ef4e2ba17b5df5d7053ea8d8517512e9944e
kuomolew <kuomolew@gmail.com>
2024-01-28 21:59:13 +0100

    add and connect CarListingByManufacturer component

---
+++ src/components/CarListings/CarListingByManufacturer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col w-full items-center my-5">
+    <car-card v-for="car in cars" :key="car.id" :car="car" class="my-5" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useCarsStore } from '@/stores/cars';
+import CarCard from '@/components/Cards/CarCard.vue';
+
+const carsStore = useCarsStore();
+
+const cars = computed(() => carsStore.FILTERED_CARS_BY_MANUFACTURER);
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <template>
   <main>
     <manufacturer-letter-selection />
-    <manufacturer-selection />
+    <div class="flex flex-row flex-nowrap w-full">
+      <manufacturer-selection />
+      <car-listing-by-manufacturer />
+    </div>
   </main>
 </template>
 
 <script lang="ts" setup>
 import ManufacturerLetterSelection from '@/components/Selection/ManufacturerLetterSelection.vue';
 import ManufacturerSelection from '@/components/Selection/ManufacturerSelection.vue';
+import CarListingByManufacturer from '@/components/CarListings/CarListingByManufacturer.vue';
 </script>
