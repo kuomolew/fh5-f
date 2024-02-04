f0ad9ca176cb3f4ef2b8c7662e7cbe0523bf3f16
kuomolew <kuomolew@gmail.com>
2024-02-04 18:29:38 +0100

    minor style update

--- src/components/CarListings/CarListingByManufacturer.vue
+++ src/components/CarListings/CarListingByManufacturer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-wrap w-full h-fit justify-around my-5 mx-10">
+  <div class="flex flex-wrap w-full h-fit justify-around py-5 px-10">
     <car-card v-for="car in cars" :key="car.id" :car="car" class="my-5" />
   </div>
 </template>
